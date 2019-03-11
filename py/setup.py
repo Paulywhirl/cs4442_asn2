b5c1a0f1a97029a73ccf41499fe75096bb89ceaa
paulywhirl <plhenderson12@hotmail.com>
2019-03-11 14:52:06 -0400

    added setup.py, changed gitignore for venv

---
+++ py/setup.py
@@ -0,0 +1,32 @@
+from __future__ import division, print_function, unicode_literals
+
+# Common imports
+import numpy as np
+import os
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+
+# To plot pretty figures
+# %matplotlib inline
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "end_to_end_project"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+# Ignore useless warnings (see SciPy issue #5998)
+import warnings
+warnings.filterwarnings(action="ignore", message="^internal gelsd")
